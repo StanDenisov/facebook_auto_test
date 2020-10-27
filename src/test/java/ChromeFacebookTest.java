323fc4b4be2c92354fb56d88875cc81428272cd5
StanDenisov <git@warlord.dev>
2020-10-27 03:07:16 +0300

    add alure + alure maven plugin

--- src/test/java/ChromeFacebookTest.java
+++ src/test/java/ChromeFacebookTest.java
@@ -1,16 +1,15 @@
 import conf.Conf;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.RegistrationPage;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.openqa.selenium.remote.ErrorCodes.TIMEOUT;
+import static io.qameta.allure.Allure.step;
 
 public class ChromeFacebookTest {
 
@@ -34,6 +33,7 @@ public class ChromeFacebookTest {
 
     @Test
     public void chromeRegistration() throws InterruptedException {
+        step("chrome registration");
         RegistrationPage registrationPage = new RegistrationPage(driver);
         driver.get(Conf.getProperty("page"));
         registrationPage.registration(1);
@@ -41,6 +41,7 @@ public class ChromeFacebookTest {
 
     @Test
     public void chromeLogin() {
+        step("chrome login");
         LoginPage loginPage = new LoginPage(driver);
         driver.get(Conf.getProperty("page"));
         loginPage.login();
--- src/test/java/FoxFacebookTest.java
+++ src/test/java/FoxFacebookTest.java
@@ -1,27 +1,15 @@
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
 import conf.Conf;
-import entitys.User;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import lombok.var;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.RegistrationPage;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.concurrent.TimeUnit;
-
-import static org.openqa.selenium.remote.ErrorCodes.TIMEOUT;
+import static io.qameta.allure.Allure.step;
 
 public class FoxFacebookTest {
 
@@ -45,6 +33,7 @@ public class FoxFacebookTest {
 
     @Test
     public void foxRegistration() throws InterruptedException {
+        step("fox registration");
         driver.get(Conf.getProperty("page"));
         RegistrationPage registrationPage = new RegistrationPage(driver);
         registrationPage.registration(1);
@@ -53,6 +42,7 @@ public class FoxFacebookTest {
 
     @Test
     public void foxLogin() {
+        step("fox login");
         driver.get(Conf.getProperty("page"));
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login();
